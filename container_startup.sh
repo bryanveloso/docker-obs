d5ac66a486a4cb2dd4569e176bea65955a4f3844
Bryan Veloso <bryan@velo.so>
2021-10-15 19:48:17 -0700

    Actually listen on the right port.

--- container_startup.sh
+++ container_startup.sh
@@ -3,13 +3,13 @@ OUR_IP=$(hostname -i)
 rm -rf /tmp/.X*
 # Start VNC server (Uses VNC_PASSWD Docker ENV variable)
 mkdir -p /tmp/.vnc && echo "$VNC_PASSWD" | vncpasswd -f > /tmp/.vnc/passwd
-vncserver :0 -localhost no -nolisten -rfbauth /tmp/.vnc/passwd -xstartup /opt/x11vnc_entrypoint.sh
+vncserver :90 -localhost no -nolisten -rfbauth /tmp/.vnc/passwd -xstartup /opt/x11vnc_entrypoint.sh
 # Start noVNC web server
-/opt/noVNC/utils/launch.sh --listen 5901 &
+/opt/noVNC/utils/launch.sh --listen 5991 &
 
 echo -e "\n\n------------------ VNC environment started ------------------"
-echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $OUR_IP:5900"
-echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$OUR_IP:5901/?password=$VNC_PASSWD\n"
+echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $OUR_IP:5990"
+echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$OUR_IP:5991/?password=$VNC_PASSWD\n"
 
 if [ -z "$1" ]; then
   tail -f /dev/null
